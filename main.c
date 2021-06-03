403f42818fc52f16e8bdac3a425187074d4d9534
KarenBassem22 <85080830+KarenBassem22@users.noreply.github.com>
2021-06-04 01:29:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,22 @@
 #include "stdint.h"
 #include "C:/Users/ub/Desktop/project/tm4c123gh6pm.h"
 #define RED 0x02
-#define BLUE 0x04
-#define GREEN 0x08
+
 void SystemInit(){}
 void initialise(void)
 {
-//uint32_t delay;
+
 SYSCTL_RCGCGPIO_R |= 0x20;
-//delay =1; //dummy variable or while loop
+
 while((SYSCTL_PRGPIO_R &0x20) ==0){}; //for delay
 GPIO_PORTF_LOCK_R = 0x4C4F434B;
-GPIO_PORTF_CR_R = 0x1F;
+GPIO_PORTF_CR_R = 0x02;
 
-GPIO_PORTF_DIR_R = 0X0E;
+GPIO_PORTF_DIR_R |= 0X02;
 GPIO_PORTF_AFSEL_R = 0;
 GPIO_PORTF_AMSEL_R = 0;
-GPIO_PORTF_DEN_R = 0x1F;
-GPIO_PORTF_PUR_R = 0x11;
+GPIO_PORTF_DEN_R = 0x02;
+GPIO_PORTF_PUR_R = 0;
 GPIO_PORTF_PCTL_R=0;
 }
 
