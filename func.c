f3c9c10f7cc0e8e50d6ffa99f821b1a536eb6de7
mayar19 <54380002+mayar19@users.noreply.github.com>
2021-06-03 22:16:45 +0200

    Add files via upload

---
+++ func.c
@@ -0,0 +1,25 @@
+#include<stdint.h>
+#include"tm4c123gh6pm.h"
+
+int main(void){
+	int distance=657;
+	int digit[3],number,i;
+
+	DDRA |=0x7F;
+	PORTA =(PORTA & 0x00) | (PORTA | 0x70);
+	while(1){
+		i=0;
+		if(distance>=100){
+			number=distance;
+			while(i<3){
+				digit[i]=number%10;
+				number=number/10;
+				i++;
+		}}
+for(i=0;i<3;i++){
+	//PORTB =(PORTB & 0xF8)| (1<<i);
+	//PORTA=(PORTA &0xF0)|digit[i];
+	PORTA=(PORTA & 0x00)|(digit[i]&0x0F)|(1<<(4+i));
+	_delay_ms(1);
+}}
+}
