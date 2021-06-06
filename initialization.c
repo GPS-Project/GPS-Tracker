7c1eebff7ce8fc440f0ba8cfe33fe5177227fb5f
Mariam Ezzat <85201077+MariamEzzat1@users.noreply.github.com>
2021-06-06 17:05:04 +0200

    Update initialization.c

--- initialization.c
+++ initialization.c
@@ -10,38 +10,35 @@ void SystemInit(){}
 	     7-segments-->PB */
 	void initialization()
 	{
-		//*************SETTING CLK FOR A,F,B**********
-		SYSCTL_RCGCGPIO_R |=0x23;
-		while ((SYSCTL_PRGPIO_R &0x23)==0){};
-		//**************LED****************
+		//SETTING CLK FOR A,F,D*
+		SYSCTL_RCGCGPIO_R |=0x29;
+		while ((SYSCTL_PRGPIO_R &0x29)==0){};
+		//*LED**
 			GPIO_PORTF_LOCK_R = UNLOCK;
 			GPIO_PORTF_CR_R  |= RED; 
-			GPIO_PORTF_AMSEL_R =0x0;
-			GPIO_PORTF_AFSEL_R =0x0;
+			GPIO_PORTF_AMSEL_R&=(~RED);
+			GPIO_PORTF_AFSEL_R &=(~RED);
 			GPIO_PORTF_DEN_R |= RED; 
 			GPIO_PORTF_PCTL_R = 0x0; //generalpurpose
 			GPIO_PORTF_DIR_R|= RED;
-			GPIO_PORTF_DATA_R= 0x00;
-		//*************UART****************
+			GPIO_PORTF_DATA_R&=(~RED);
+			//*7-SEGMENTS*
+			GPIO_PORTD_LOCK_R|= UNLOCK;
+			GPIO_PORTD_CR_R  |=0xFF;
+			GPIO_PORTD_AMSEL_R =0x00;
+			GPIO_PORTD_AFSEL_R =0x00;
+			GPIO_PORTD_PCTL_R =0x00;
+			GPIO_PORTD_DEN_R |=0xFF;
+			GPIO_PORTD_DIR_R|=0xFF;
+			GPIO_PORTD_DATA_R=0x00;	
+
 			GPIO_PORTA_LOCK_R|= UNLOCK;
-			GPIO_PORTA_CR_R  |= UART;
+			GPIO_PORTA_CR_R  |=0xFF;
 			GPIO_PORTA_AMSEL_R =0x00;
-			GPIO_PORTA_AFSEL_R =UART;
-			GPIO_PORTA_PCTL_R =0x11;  
-			GPIO_PORTA_DEN_R |=0x03;
-		//************7-SEGMENTS**************
-			GPIO_PORTB_LOCK_R|= UNLOCK;
-			GPIO_PORTB_CR_R  |=0xFF;
-			GPIO_PORTB_AMSEL_R =0x00;
-			GPIO_PORTB_AFSEL_R =0x00;
-			GPIO_PORTB_PCTL_R =0x00;
-			GPIO_PORTB_DEN_R |=0xFF;
-			GPIO_PORTB_DIR_R|=0xFF;
-			GPIO_PORTB_DATA_R=0x70;
-			
-	}
-	int main(){
-		initialization ();
-		GPIO_PORTF_DATA_R |=0x02;
-		
+			GPIO_PORTA_AFSEL_R =0x00;
+			GPIO_PORTA_PCTL_R =0x00;
+			GPIO_PORTA_DEN_R |=0xFF;
+			GPIO_PORTA_DIR_R|=0xFF;
+			GPIO_PORTA_DATA_R=0x70;	
+
 	}
