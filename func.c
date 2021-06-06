b2cd90cfcd150ae4b740ebb316ad9560b824f717
mayar19 <54380002+mayar19@users.noreply.github.com>
2021-06-06 16:34:53 +0200

    Update func.c

--- func.c
+++ func.c
@@ -2,21 +2,28 @@
 #include"tm4c123gh6pm.h"
 
 void segment(int d){
-	int distance=657;
-	int digit[3],number,i;
-	GPIO_PORTB_DATA_R =(GPIO_PORTB_DATA_R & 0x00) | (GPIO_PORTB_DATA_R | 0x70);
-	while(1){
+	int digit[3],number,i,j;
+	GPIO_PORTD_DATA_R =(GPIO_PORTD_DATA_R & 0x00) | (GPIO_PORTD_DATA_R | 0x70);
 		i=0;
-		if(distance>=100){
-			number=distance;
-			while(i<3){
+		if(d>100)
+			{
+	
+			number=d;
+			while(i<3)
+				{
 				digit[i]=number%10;
 				number=number/10;
 				i++;
-		}}
-for(i=0;i<3;i++){
-	GPIO_PORTB_DATA_R=(GPIO_PORTB_DATA_R & 0x00)|(digit[i]&0x0F)|(1<<(4+i));
-	SysCtlDelay(10);
-}}
+	    	}
+			}
+for(i=0;i<3;i++)
+	{
+
+		GPIO_PORTA_DATA_R=(GPIO_PORTA_DATA_R & 0X0F) | (1<<(4+i));
+
+                GPIO_PORTD_DATA_R=(GPIO_PORTD_DATA_R & 0XF0) | (digit[i]&0x0F);
+//for delay
+		for(j=0;j<100;j++){}
+  }
 }
 
