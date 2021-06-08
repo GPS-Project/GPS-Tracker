108b05bc220c874e6a31915102ff74b769aa306d
mayar19 <54380002+mayar19@users.noreply.github.com>
2021-06-09 00:34:20 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,26 @@
 #include"func.h"
+int flag=0;
+int prev_values [N];
 
 int main() {
 int d;	
+initialization();
 while (1)
 {
-initialization();
-d=distance(30.5,30.9,32.5,32.2);
-
-LED(d);
-Segment(d);
+	delay_1sec();
+	//read function
+	//trimmed func
+	if(flag==0){
+	
+		//prev_values
+		flag=1;}
+	else if(flag==1){
+	//d=distance(prev_values[0],prev_values[1],,)}
+		if(d==-1){
+		skip;}
+		else { led(d);
+		      segment(d);}
+		if(d>100){
+		break;}
 }
 	}
