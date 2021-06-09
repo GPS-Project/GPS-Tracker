d32e2db21e271187cdae55414ed386f20cd22ca7
Mariam Ezzat <85201077+MariamEzzat1@users.noreply.github.com>
2021-06-09 16:48:06 +0200

    Add files via upload

---
+++ read.c
@@ -0,0 +1,25 @@
+#include "C:/Users/El.Takwa/Desktop/proj/tm4c123gh6pm.h"
+#include <stdbool.h>
+#define MAX 100 //max number of characters recieved ftom gps module
+unsigned char data ;
+unsigned int i;
+bool stop= false;
+unsigned char stringdata [MAX];
+char uart_read (void){
+	while ((UART2_FR_R &0x10)!=0){
+	 data = UART2_DR_R&0xFF;
+	}
+   return data;
+}
+void readmsg (){
+
+	do{
+	if (data == '$'){
+		
+	}
+	
+	}
+	
+while (1);
+
+	}
