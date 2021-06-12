d22c85620ce4b4e72576673bb6b56d2d923f054e
mayar19 <54380002+mayar19@users.noreply.github.com>
2021-06-12 21:10:37 +0200

    Update func.c

--- func.c
+++ func.c
@@ -112,3 +112,55 @@ void delay_1sec(void){
        unsigned long i;   
        for(i = 0; i <= 3000000; i++ ){};
 		}
+void UART0_Init(void){
+    SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R0;
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;
+    
+    UART0_CTL_R &= ~UART_CTL_UARTEN;
+    //set buad rate devider
+    UART0_IBRD_R = 104;
+    UART0_FBRD_R = 11;
+		UART0_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN);
+    UART0_CTL_R |= (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);
+
+    GPIO_PORTA_AFSEL_R |= 0x03;
+    GPIO_PORTA_PCTL_R = (GPIO_PORTA_PCTL_R & ~0xFF) | (GPIO_PCTL_PA0_U0RX | GPIO_PCTL_PA1_U0TX);
+    GPIO_PORTA_DEN_R |= 0x03;
+}
+
+char UART0_read(void){
+    while((UART0_FR_R&0x10) == 0x10);
+    return UART0_DR_R & 0xFF;
+}
+
+void UART0_write(char c){
+    while((UART0_FR_R & UART_FR_TXFF) != 0);
+    UART0_DR_R = c;
+}
+
+void UART1_Init(void){
+    SYSCTL_RCGCUART_R |= 0x02;
+    while((SYSCTL_PRUART_R & 0x02) == 0);
+    SYSCTL_RCGCGPIO_R |= 0x02;
+		while((SYSCTL_PRGPIO_R & 0x02) == 0);
+	
+	  GPIO_PORTB_CR_R |= 0x1F;
+    GPIO_PORTB_AMSEL_R &= ~0x1F;
+    GPIO_PORTB_AFSEL_R |= 0x03;
+    GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & ~0xFF) | (GPIO_PCTL_PB0_U1RX | GPIO_PCTL_PB1_U1TX);
+    GPIO_PORTB_DEN_R |= 0x1F;
+    GPIO_PORTB_DIR_R |= 0x1E;
+    GPIO_PORTB_DIR_R &= ~0x01;
+    
+    UART1_CTL_R &= ~UART_CTL_UARTEN;
+    //set buad rate devider
+    UART1_IBRD_R = 104;
+    UART1_FBRD_R = 11;
+	  UART1_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN);
+    UART1_CTL_R |= (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);
+}
+
+char UART1_read(void){
+    while((UART1_FR_R & 0x10) == 0x10);
+    return UART1_DR_R & 0xFF;
+}
