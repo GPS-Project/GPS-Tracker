6c1952a0726266f8785d1266d9b1d7f1b03f1e6c
KarenBassem22 <85080830+KarenBassem22@users.noreply.github.com>
2021-06-15 01:07:50 +0200

    Create function.h

---
+++ function.h
@@ -0,0 +1,35 @@
+#include "stdlib.h"
+#include "stdint.h"
+#include "C:/Keil/tm4c123gh6pm.h"
+
+
+//masks;
+#define UNLOCK 0x4C4F434B
+#define RED 0x02
+#define UART 0xC0
+#define N 2
+#include <math.h>
+#define pi 3.14159265358979323846
+
+double prevA;
+double prevB;
+char lat[20];                    // latitude array
+char lg[20];                     // longitude array
+void SystemInit();
+void initialization();
+void LED(double );
+void Segment(int );
+double deg2rad(double);
+double rad2deg(double);
+double distance(double , double , double , double );
+void delay_1sec(void);
+char UART1_read(void);
+void UART1_Init(void);
+void UART0_write(char c);
+char UART0_read(void);
+void UART0_Init(void);
+void gps();
+double longitude(char[]);
+double latitude(char[]);
+double ConvertA(double , double , double);
+double ConvertB(double , double , double);
